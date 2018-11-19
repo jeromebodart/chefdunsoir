efde9bd9eb195dc8e0770308e0ce432ef7ef1579
bodart <jerome.bodart@hei.yncrea.fr>
2018-11-19 15:56:54 +0100

    first migration + model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,82 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_19_135028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "indisponibilities", force: :cascade do |t|
+    t.string "day"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_indisponibilities_on_restaurant_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "capicity"
+    t.string "category"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.integer "priceperday"
+    t.string "photos"
+    t.boolean "monday"
+    t.boolean "tuesday"
+    t.boolean "wednesday"
+    t.boolean "thursday"
+    t.boolean "friday"
+    t.boolean "saturday"
+    t.boolean "sunday"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "stars"
+    t.boolean "chef"
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.string "reviewable_id"
+    t.string "reviewable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_reviews_on_receiver_id"
+    t.index ["reviewable_id", "reviewable_type"], name: "index_reviews_on_reviewable_id_and_reviewable_type"
+    t.index ["sender_id"], name: "index_reviews_on_sender_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.text "description"
+    t.string "photo"
+    t.string "password"
+    t.integer "phonenumber"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "indisponibilities", "restaurants"
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "restaurants", "users"
+  add_foreign_key "reviews", "users", column: "receiver_id"
+  add_foreign_key "reviews", "users", column: "sender_id"
 end
