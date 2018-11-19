a90ed706b94d999ccbb860154ac7d0de888e081d
RenaudinP <renaudin.pierre@gmail.com>
2018-11-19 18:27:39 +0100

    fichier seed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :reservation
+  belongs_to :sender, class_name: 'User'
   belongs_to :reviewable, polymorphic: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,107 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Review.destroy_all
+Reservation.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+user1 = User.create!(email: "email1@example.com", password: 'azerty')
+user2 = User.create!(email: "email2@example.com", password: 'azerty')
+user3 = User.create!(email: "email3@example.com", password: 'azerty')
+
+
+puts 'Creating restaurants...'
+resto_fr = Restaurant.create!(
+  {
+    name:           'le français',
+    capicity:       '3',
+    category:       'french',
+    address:        '51 rue genri gorjus',
+    longitude:      '4.850000',
+    latitude:       '45.750000',
+    priceperday:    '45',
+    monday:         'true',
+    tuesday:        'true',
+    wednesday:      'false',
+    thursday:       'false',
+    friday:         'false',
+    saturday:       'false',
+    sunday:         'false',
+    user:           user1
+  })
+
+resto_be = Restaurant.create!(
+  {
+    name:           'le chinois',
+    capicity:       '5',
+    category:       'chinois',
+    address:        '54 rue genri gorjus',
+    longitude:      '4.850000',
+    latitude:       '45.750000',
+    priceperday:    '46',
+    monday:         'true',
+    tuesday:        'true',
+    wednesday:      'false',
+    thursday:       'false',
+    friday:         'true',
+    saturday:       'false',
+    sunday:         'false',
+    user:           user1
+  })
+
+resto_ch = Restaurant.create!(
+  {
+    name:           'le belge',
+    capicity:       '4',
+    category:       'french',
+    address:        '52 rue de belgique',
+    longitude:      '4.850000',
+    latitude:       '45.750000',
+    priceperday:    '45',
+    monday:         'true',
+    tuesday:        'true',
+    wednesday:      'true',
+    thursday:       'false',
+    friday:         'false',
+    saturday:       'false',
+    sunday:         'false',
+    user:           user2
+  })
+
+puts 'Finished restuarant!'
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    description:         'très bon restaurant avec une équipe sympas',
+    stars:               '2',
+    chef:                'true',
+    reviewable:          resto_fr,
+    sender:              user2
+  },
+  {
+    description:         'très bon restaurant avec une équipe sympas',
+    stars:               '4',
+    chef:                'true',
+    reviewable:          resto_fr,
+    sender:              user3
+  },
+  {
+    description:         'très bon restaurant avec une équipe sympas',
+    stars:               '3',
+    chef:                'true',
+    reviewable:          resto_be,
+    sender:              user1
+  },
+]
+Review.create!(reviews_attributes)
+puts 'Finished reviews!'
+
+
+
+
+
+
+
+
+
