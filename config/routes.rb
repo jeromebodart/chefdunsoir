3ea58a171b44e87d500b7dfc338db7af66af2cf8
Hugo Joly <yh.joly@gmail.com>
2018-11-19 17:17:26 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
-
-
-
   devise_for :users
   root to: 'pages#home'
+  resources :restaurants do
+    resources :reservations
+  end
+  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
