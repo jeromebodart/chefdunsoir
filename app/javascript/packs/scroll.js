61a8ca5d632a81c7f0da1204624e69c24cde2f95
RenaudinP <renaudin.pierre@gmail.com>
2018-11-21 16:26:03 +0100

    javascript fin

--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -4,7 +4,8 @@ const scroll = () => {
   const reservation = document.querySelector('.reserver');
   const element = document.querySelector('#container-restaurant-cards')
   if (reservation) {
-    reservation.addEventListener('click', function() {
+    reservation.addEventListener('click', function(e) {
+      e.preventDefault();
       console.log("click");
       const topPosition = element.offsetTop - document.querySelector('.navbar-wagon').offsetHeight
       window.scroll({top: topPosition, behavior: "smooth"})
