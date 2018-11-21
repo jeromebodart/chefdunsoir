1dff179a71c0490d4f9c0f7e05fa399d02b1df8d
RenaudinP <renaudin.pierre@gmail.com>
2018-11-21 15:29:09 +0100

    javascript + css restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'restaurants#index'
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :restaurants do
-    resources :reservations, only: [:new]
+    resources :reservations, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
