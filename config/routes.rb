ac48873ec51c1b9d1d2d81d34bfbb69a685092fa
Hugo Joly <yh.joly@gmail.com>
2018-11-22 16:44:11 +0100

    add profile restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :profile do
     resources :reservations, only: [:index, :destroy]
+    resources :restaurants, only: [:index, :show]
   end
 
 end
