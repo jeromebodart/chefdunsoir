0df465070dfa7ce421220f81d06cb1f642f0599e
Hugo Joly <yh.joly@gmail.com>
2018-11-22 12:15:04 +0100

    add date, restaurant, and css to each reservation profile

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
-  has_many :pictures, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
 end
