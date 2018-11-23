7491f04f945054e8e48c3578e2c5c1618096cfe4
bodart <jerome.bodart@hei.yncrea.fr>
2018-11-23 15:15:31 +0100

    debug map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,6 +5,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const mapElement = document.getElementById('map');
 
+
 if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
